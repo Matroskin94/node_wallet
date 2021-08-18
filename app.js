2ef8b4fc1bfc945f24331097c151cd8c82bd6daf
VTB4042782 <ankotov@dev.vtb>
2021-08-18 16:27:34 +0300

    [BACK-2] - Added connection to atlas remote database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -10,11 +11,32 @@ app.use(morgan('tiny'));
 
 const port = process.env.PORT;
 const api = process.env.API_URL;
+const dbUser = process.env.DB_USER;
+const userPass = process.env.DB_PASS;
+const dbName = process.env.DB_NAME;
 
-app.get('/', (req, res) => {
+const dbConfig = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName
+};
+
+mongoose
+  .connect(
+    `mongodb+srv://${dbUser}:${userPass}@cluster0.bazwy.mongodb.net/${dbName}?retryWrites=true&w=majority`,
+    dbConfig
+  )
+  .then(() => {
+    console.log('DATABASE CONNECTED');
+  })
+  .catch(() => {
+    console.log('DATABASE CONNECTION ERROR');
+  });
+
+app.get(`${api}/`, (req, res) => {
   res.send('Hello API');
 });
 
 app.listen(port, () => {
-  console.log("Server is running on port: " + port);
+  console.log("SERVER IS RUNNING ON PORT: " + port);
 });
