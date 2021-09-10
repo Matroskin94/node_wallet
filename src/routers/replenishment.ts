e94c2bfb538308a576d38dd8c5cd79c04d0dc0bb
VTB4042782 <ankotov@dev.vtb>
2021-09-10 10:02:36 +0300

    [BACK-9] - Added get/delete methods for concrete wastea and replenishment

--- src/routers/replenishment.ts
+++ src/routers/replenishment.ts
@@ -41,4 +41,26 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  const replenishment = await Replenishment.findById(req.params.id).populate('currency type');
+
+  if (replenishment) {
+    res.status(200).send(replenishment);
+  } else {
+    res.status(500).json({ message: 'Replenishment with given ID not found'})
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  Replenishment.findByIdAndRemove(req.params.id).then(replenishment => {
+    if (replenishment) {
+      return res.status(200).json({success: true, message: 'Replenishment was deleted'});
+    } else {
+      return res.status(404).json({success: false, message: 'Replenishment not deleted'})
+    }
+  }).catch(err => {
+    return res.status(400).json({success: false, err})
+  });
+});
+
 export { router };
--- src/routers/wastes.ts
+++ src/routers/wastes.ts
@@ -42,4 +42,26 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  const waste = await Waste.findById(req.params.id).populate('currency type');
+
+  if (waste) {
+    res.status(200).send(waste);
+  } else {
+    res.status(500).json({ message: 'Waste type with given ID not found'})
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  Waste.findByIdAndRemove(req.params.id).then(waste => {
+    if (waste) {
+      return res.status(200).json({success: true, message: 'waste was deleted'});
+    } else {
+      return res.status(404).json({success: false, message: 'waste not deleted'})
+    }
+  }).catch(err => {
+    return res.status(400).json({success: false, err})
+  });
+});
+
 export { router };
